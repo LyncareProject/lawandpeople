90cca862bcad4457989f43e097361331c7f141df
kun woo kim <kimkuns98@gmail.com>
2023-07-17 14:35:21 +0900

    수정

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,7 +1,7 @@
 import './SideBar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faX } from '@fortawesome/free-solid-svg-icons'
-
+import mainlogo from '../../assets/images/mainlogo.png'
 
 const SideBar = ({ sideBar, setSideBar })=>{
     const handleSideBar =()=>{
@@ -16,11 +16,12 @@ const SideBar = ({ sideBar, setSideBar })=>{
             <div className='btnWrap'>
                 <button className='sideCloseBtn' onClick={ handleSideBar }><FontAwesomeIcon icon={ faX } /></button>
             </div>
-            <h2 className='logo'>법무법인 법과 사람들</h2>
-            <a className='menu' href="/counsel">상담 사례</a>
-            <a className='menu' href="/cases">해결 사례</a>
-            <a className='menu' href="/guide">법률 가이드</a>
-            <a className='menu' href="/video">법률 동영상</a>
+            <img className='logo' src={ mainlogo } alt="mainlogo" />
+            <a className='menu' href="/about">- 사무소 소개</a>
+            <a className='menu' href="/counsel">- 상담 사례</a>
+            <a className='menu' href="/cases">- 해결 사례</a>
+            <a className='menu' href="/guide">- 법률 가이드</a>
+            <a className='menu' href="/video">- 법률 동영상</a>
             <a className='counsel' href="/counsel/write">상담 신청하기</a>
         </div>
     )   
